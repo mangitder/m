d9d09196616894481e1951b2f144d09298bfeba3
mangitder <654285007@webmail.npru.ac.th>
2024-03-10 19:37:53 +0700

    ๅ

--- Home.py
+++ Home.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-st.page_link("Home.py", label="Prediction", icon="🏠")
-st.page_link("pages/DTree.py", label="Prediction on DecisionTree", icon="1️")
-st.page_link("pages/NaiveBaye.py", label="Prediction on Naive Baye", icon="2️")
+st.page_link("Home.py", label="Predictionฟฟฟ", icon="🏠")
+st.page_link("pages/DecisionTree.py.py", label="Prediction on DecisionTree", icon="🏠")
+st.page_link("pages/NaiveBaye.py", label="Prediction on Naive Baye", icon="🏠")
 st.page_link("http://www.google.com", label="Google", icon="🌎")
-st.header("2")
+st.header("100")
 
 
