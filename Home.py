838bdfb7a052524f6d776710449ea90ed5d97d68
mangitder <654285007@webmail.npru.ac.th>
2024-03-10 11:16:02 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,29 +7,3 @@ st.page_link("http://www.google.com", label="Google", icon="🌎")
 st.header("2")
 
 
-import streamlit as st
-
-# Define page functions
-def home_page():
-    st.title("Home Page")
-    st.write("Welcome to the Home Page!")
-
-def page1():
-    st.title("Page 1")
-    st.write("This is Page 1")
-
-def page2():
-    st.title("Page 2")
-    st.write("This is Page 2")
-
-# Sidebar navigation
-st.sidebar.title("Navigation")
-selection = st.sidebar.radio("Go to", ["Home", "Page 1", "Page 2"])
-
-# Display the selected page
-if selection == "Home":
-    home_page()
-elif selection == "Page 1":
-    page1()
-elif selection == "Page 2":
-    page2()
