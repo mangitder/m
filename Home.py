6e2e054086469f711d9251efd5d7a710b1950563
mangitder <654285007@webmail.npru.ac.th>
2024-03-11 08:42:30 +0700

    d

--- Home.py
+++ Home.py
@@ -4,7 +4,7 @@ st.page_link("Home.py", label="Predictionฟฟฟ", icon="🏠")
 st.page_link("pages/DecisionTree.py", label="Prediction on DecisionTree", icon="🏠")
 st.page_link("pages/NaiveBaye.py", label="Prediction on Naive Baye", icon="🏠")
 st.page_link("http://www.google.com", label="Google", icon="🌎")
-st.header("wsssss")
+st.header("w4")
 
 
 
