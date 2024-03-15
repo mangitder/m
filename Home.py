85d165d715a5a214c6bec51bb9f3d5eb194803c5
mangitder <654285007@webmail.npru.ac.th>
2024-03-15 14:14:13 +0700

    q

--- Home.py
+++ Home.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.page_link("Home.py", label="Prediction", icon="🏠")
 st.page_link("pages/DecisionTree.py", label="Prediction on DecisionTree", icon="🏠")
 st.page_link("pages/NaiveBaye.py", label="Prediction on Naive Baye", icon="🏠")
+st.page_link("pages/K-mean.py", label="Prediction on K-mean", icon="🏠")
 st.page_link("http://www.google.com", label="Google", icon="🌎")
 st.header("d")
 
